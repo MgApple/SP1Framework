5f3e9387d1ca1385fd218a05824a700d0d1dfe5c
MgApple <jiabao14jk@gmail.com>
2020-08-19 11:28:02 +0800

    added cop

---
+++ SP1Framework/Cop.cpp
@@ -0,0 +1,17 @@
+#include "Cop.h"
+
+Cop::Cop() : Enemy(TYPE::TYPE_COP)
+{
+	setPos('x', rand() % 80);
+	setPos('y', rand() % 25);
+}
+
+Cop::~Cop()
+{
+	//nothing
+}
+
+void move()
+{
+	//overload func, nothing
+}
