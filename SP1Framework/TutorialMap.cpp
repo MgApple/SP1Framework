e819847eecf85c455858120d3a8835baba4b948f
MgApple <jiabao14jk@gmail.com>
2020-08-20 15:37:47 +0800

    added tutorial class (incomplete)

---
+++ SP1Framework/TutorialMap.cpp
@@ -0,0 +1,1 @@
+#include "TutorialMap.h"
---
+++ SP1Framework/TutorialMap.h
@@ -0,0 +1,14 @@
+#pragma once
+class TutorialMap
+{
+private:
+	char map[24][50]
+
+public:
+	TutorialMap();
+	~TutorialMap();
+	int GetEntity();
+	void SetEntity();
+
+};
+
