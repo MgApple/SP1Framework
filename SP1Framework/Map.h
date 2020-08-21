74027465351fb14274b2729460fd0a7dd07f3c64
MgApple <jiabao14jk@gmail.com>
2020-08-21 11:20:37 +0800

    added title and seperated func for loading tutorial, removed tutorialmap class

--- SP1Framework/Map.h
+++ SP1Framework/Map.h
@@ -3,10 +3,12 @@ class Map
 {
 private:
 	char map[24][80];
+	char tutorial[24][50];
 public:
 	Map(void);
 	~Map(void);
 	int getEntity(int x, int y);
 	void setEntity(int x, int y, int e);
 	void loadMap(void);
+	void loadtutorial(void);
 };
--- SP1Framework/TutorialMap.cpp
+++
@@ -1,1 +0,0 @@
-#include "TutorialMap.h"
