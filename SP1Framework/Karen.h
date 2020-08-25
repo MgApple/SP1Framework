02c07063b17985f3a4da21c7035446c31554c15e
Noraa-kcab <68600500+Noraa-kcab@users.noreply.github.com>
2020-08-25 17:57:22 +0800

    Made Karen able to target player
    im not sure wat the rest of this is

--- SP1Framework/Karen.h
+++ SP1Framework/Karen.h
@@ -1,16 +1,36 @@
 #pragma once
 #include "Enemy.h"
-#include "game.h"
+#include <vector>
+#include <list>
+#include <math.h>
+
 class Karen :public Enemy
 {
 private:
 	bool aggrocheck;
-	int LRcheck();
-	int UDcheck();
+	struct Node
+	{
+		bool bObstacle = false;
+		bool bVisited = false;
+		float globalGoal = INFINITY; // INFINITY is a float type
+		float localGoal = INFINITY;
+		COORD pos = { 0, 0 };
+		std::vector<Node*> neighbours;
+		Node* parent = NULL;
+	};
+
+	Node* nodes = NULL;
+	int consoleWidth = 80;
+	int consoleHeight = 25; // might be 24
+
+	Node* start = NULL;
+	Node* end = NULL;
 public:
 	Karen();
 	~Karen();
 	bool aggro(Entity* player,Map &map);
-	void move(Map& map, const double dt);
+	void createPath(Map& map);
+	bool solveAStar(Map& map);
+	void movement(Map& map, const double dt);
 };
 
